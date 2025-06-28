a260b0f7f17e77ca81d74442d2462ee082c5fc38
Sri Harsha <sriharshaanaparthi@gmail.com>
2025-06-28 17:55:21 +0530

    making context for favorite movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,28 @@
-import "../css/MovieCard.css"
+import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({ movie }) {
-  function onFavoriteClicl() {
-    alert("clicked");
+  const { isFavorite, addToFavorite, removeFromFavorite } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClicl(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorite(movie.id);
+    else addToFavorite(movie);
   }
 
   return (
     <div className="movie-card">
-
       <div className="movie-poster">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overplay">
-          <button className="favorite-btn" onClick={onFavoriteClicl}>
+          <button
+            className={`favorite-btn ${favorite}? "active" :""`}
+            onClick={onFavoriteClicl}
+          >
             🤍
           </button>
         </div>
@@ -21,9 +32,8 @@ function MovieCard({ movie }) {
         <h3>{movie.title}</h3>
         <p>{movie.release_date?.split("-")[0]}</p>
       </div>
-
     </div>
   );
 }
 
-export default MovieCard
+export default MovieCard;
