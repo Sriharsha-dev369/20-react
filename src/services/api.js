8835fb7ffd3f3d752144142205fbd95a108a71ee
Sri Harsha <sriharshaanaparthi@gmail.com>
2025-06-28 14:34:29 +0530

    first commit of project

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "f95ea3654fb337d469c0e7b54412bf13"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
